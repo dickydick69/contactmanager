b6a0cd5804cb53e2cc8b5f2e5391a16e30219918
unknown <pauluz.andreyz47@gmail.com>
2019-05-23 12:09:33 +0700

    Updates

---
+++ precache-manifest.434815818b2eef7011fd6b5cb7dd5ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35463d34d3dde6b5978d52a273b6ed60",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6aa6a712772346fb9b50",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3497f081ce9e0d97935d",
+    "url": "/contactmanager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6aa6a712772346fb9b50",
+    "url": "/contactmanager/static/js/2.6e4ca09c.chunk.js"
+  },
+  {
+    "revision": "3497f081ce9e0d97935d",
+    "url": "/contactmanager/static/js/main.da47a397.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
